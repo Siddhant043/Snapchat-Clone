d12ab4ec2c15694dfb1536f6d4446f35db760397
Siddhant Mishra <siddhant48311@gmail.com>
2021-03-17 14:00:22 +0530

    Login feature and chat features added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import WebcamCapture from "./WebcamCapture";
+import Preview from "./Preview";
+import Chats from "./Chats";
+import ChatView from "./ChatView";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "./features/appSlice";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <h1>Snapchat Clone!</h1>
-      <WebcamCapture />
+    <div className="app">
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app__body">
+            <Switch>
+              <Route exact path="/" component={WebcamCapture} />
+              <Route exact path="/preview" component={Preview} />
+              <Route exact path="/chats" component={Chats} />
+              <Route exact path="/chats/view" component={ChatView} />
+            </Switch>
+          </div>
+        )}
+      </Router>
     </div>
   );
 }
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,6 +1,10 @@
 import React, { useRef, useCallback, useState } from "react";
 import Webcam from "react-webcam";
+import { useDispatch } from "react-redux";
 import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
+import { setCameraImage } from "./features/cameraSlice";
+import { useHistory } from "react-router";
+import "./WebcamCapture.css";
 
 const videoConstraints = {
   width: 250,
@@ -10,12 +14,13 @@ const videoConstraints = {
 
 const WebcamCapture = () => {
   const webcamRef = useRef(null);
-  const [image, setImage] = useState(null);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
-    console.log(imageSrc);
-    setImage(imageSrc);
+    dispatch(setCameraImage(imageSrc));
+    history.push("/preview");
   }, [webcamRef]);
 
   return (
@@ -34,7 +39,6 @@ const WebcamCapture = () => {
         onClick={capture}
         fontSize="large"
       />
-      <img src={image} alt="" />
     </div>
   );
 };
